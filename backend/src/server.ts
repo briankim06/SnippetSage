02791bc564e8162d369aba9e32a34f8bd3e7f792
Brian Kim <bkim45606@gmail.com>
2025-07-16 00:32:38 -0500

    added login controller, routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,20 +2,26 @@ import express from 'express';
 import connectDB from './config/db';
 import snippetsRoutes from './routes/snippetsRoutes';
 import registerRoutes from './routes/registerRoutes';
+import loginRoutes from './routes/loginRoutes';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 
 app.use(express.json());
 app.use('/api/snippets', snippetsRoutes)
 app.use('/api/register', registerRoutes)
+app.use('/api/login', loginRoutes)
 
 // app.get('/api/notes', (req, res) => {
 //     res.send('Hello World');
 // });
 
-connectDB().then(() => app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+connectDB().then(() => app.listen(PORT, () => {
+    console.log(`Server is running on port ` + PORT);
 }));
 
 
